cadfcea0317633a3f5cf2157690ee6a9c70d4b99
harry <harry@HARRY-BARNES-PC>
2025-08-25 15:05:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int number1;
+int number2;
+int quotient;
+int remainder;
+
+Console.WriteLine("Enter first number:");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+number2 = Convert.ToInt32(Console.ReadLine());
+quotient = number1 / number2;
+remainder = number1 % number2;
+Console.WriteLine($"The quotient of the numbers is {quotient}, their remainder is {remainder}.");
